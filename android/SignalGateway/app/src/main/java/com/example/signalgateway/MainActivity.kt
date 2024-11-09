0938d749275f374d161e50ff52a2e58fd6905f9d
Tianda Huang <37414124+tiandahuang@users.noreply.github.com>
2024-11-09 09:06:32 -0800

    hello world webserver

---
+++ android/SignalGateway/app/src/main/java/com/example/signalgateway/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.signalgateway
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+// ktor imports
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        embeddedServer(Netty, 5000) {
+            install(ContentNegotiation) {
+                gson {}
+            }
+            routing {
+                get("/") {
+                    call.respond(mapOf("message" to "Hello world"))
+                }
+            }
+        }.start(wait = true)
+    }
+}
